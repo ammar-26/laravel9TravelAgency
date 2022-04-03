c69ac06dbd97e4ac4f3972ba00caa63014f2efb1
ammar_26 <ammarnady12@gmail.com>
2022-04-04 02:27:25 +0300

    first commit

---
+++ resources/views/layouts/frontbase.blade.php
@@ -0,0 +1,94 @@
+
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>@yield("title")</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="Free HTML5 Template by FREEHTML5.CO" />
+	<meta name="keywords" content="free html5, free template, free bootstrap, html5, css3, mobile first, responsive" />
+	<meta name="author" content="FREEHTML5.CO" />
+
+<!-- 
+	//////////////////////////////////////////////////////
+
+	FREE HTML5 TEMPLATE 
+	DESIGNED & DEVELOPED by FREEHTML5.CO
+		
+	Website: 		http://freehtml5.co/
+	Email: 			info@freehtml5.co
+	Twitter: 		http://twitter.com/fh5co
+	Facebook: 		https://www.facebook.com/fh5co
+
+	//////////////////////////////////////////////////////
+-->
+
+<!-- Facebook and Twitter integration -->
+	<meta property="og:title" content=""/>
+	<meta property="og:image" content=""/>
+	<meta property="og:url" content=""/>
+	<meta property="og:site_name" content=""/>
+	<meta property="og:description" content=""/>
+	<meta name="twitter:title" content="" />
+	<meta name="twitter:image" content="" />
+	<meta name="twitter:url" content="" />
+	<meta name="twitter:card" content="" />
+
+	<!-- Place favicon.ico and apple-touch-icon.png in the root directory -->
+	<link rel="shortcut icon" href="favicon.ico">
+
+	<link href='https://fonts.googleapis.com/css?family=Open+Sans:400,700,300' rel='stylesheet' type='text/css'>
+	
+	<!-- Animate.css -->
+	<link rel="stylesheet" href="{{asset('assets')}}/css/animate.css">
+	<!-- Icomoon Icon Fonts-->
+	<link rel="stylesheet" href="{{asset('assets')}}/css/icomoon.css">
+	<!-- Bootstrap  -->
+	<link rel="stylesheet" href="{{asset('assets')}}/css/bootstrap.css">
+	<!-- Superfish -->
+	<link rel="stylesheet" href="{{asset('assets')}}/css/superfish.css">
+	<!-- Magnific Popup -->
+	<link rel="stylesheet" href="{{asset('assets')}}/css/magnific-popup.css">
+	<!-- Date Picker -->
+	<link rel="stylesheet" href="{{asset('assets')}}/css/bootstrap-datepicker.min.css">
+	<!-- CS Select -->
+	<link rel="stylesheet" href="{{asset('assets')}}/css/cs-select.css">
+	<link rel="stylesheet" href="{{asset('assets')}}/css/cs-skin-border.css">
+	
+	<link rel="stylesheet" href="{{asset('assets')}}/css/style.css">
+
+
+	<!-- Modernizr JS -->
+	<script src="{{asset('assets')}}/js/modernizr-2.6.2.min.js"></script>
+	<!-- FOR IE9 below -->
+	<!--[if lt IE 9]>
+	<script src="js/respond.min.js"></script>
+	<![endif]-->
+@yield("head")
+</head>
+<body>
+
+        @include("home.header")
+
+
+        @section('sidebar')
+            @include("home.sidebar")
+        @show
+
+        
+        @section('slider')
+            @include("home.slider")
+        @show
+
+
+            @yield('content')
+
+        @include("home.footer")
+
+        @yield('foot')
+    </body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,70 @@
+<?php
+
+use App\Http\controllers\HomeController;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminPanel\HomeController as AdminHomeController;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+
+//1- Do something in the route 
+Route::get('/flight',function(){
+    return view('welcome');
+});
+
+Route::get('/hotel',function(){
+    return view('welcome');
+});
+
+Route::get('/car',function(){
+    return view('welcome');
+});
+
+
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+//2- Call view in route
+
+Route::get('/welcome', function () {
+    return view('welcome');
+});
+
+//3- Call Controller Function
+
+Route::get('/', [HomeController::class,'index'])->name('home');
+
+//4- Route-> Controller->View
+
+Route::get('/test', [HomeController::class,'test'])->name('test');
+
+//5- Route with parameters
+Route::get('/param/{id}/{number}', [HomeController::class,'param'])->name('test');
+
+//6- Route with post 
+Route::post('/save', [HomeController::class,'save'])->name('save');
+
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
+
+//******* ADMIN PANEL ROUTES *******
+
+Route::get('/admin', [AdminHomeController::class,'index'])->name('admin');
